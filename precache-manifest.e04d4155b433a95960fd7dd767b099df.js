2b803d42966103b605b3490781553b28575bd7b4
Wyfy0107 <nguyenduy010798@gmail.com>
2020-06-29 01:46:07 +0300

    Updates

---
+++ precache-manifest.e04d4155b433a95960fd7dd767b099df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ccd57949e238ecc8d7c5ceebebee97",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "818104d583583efa9b27",
+    "url": "/weather-app/static/css/main.1bcfd8e8.chunk.css"
+  },
+  {
+    "revision": "e09da467b914426fb84f",
+    "url": "/weather-app/static/js/2.96b1c89f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.96b1c89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818104d583583efa9b27",
+    "url": "/weather-app/static/js/main.55a4856c.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "7175ecfbb792eb8869df700916370997",
+    "url": "/weather-app/static/media/weather3.7175ecfb.jpg"
+  }
+]);
